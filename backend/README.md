fb4de1a5ca108f9f6ddf51c1327ddcb90bce0b5c
VaishvikMaisuria <vaishvik.maisuria@gmail.com>
2022-10-23 20:05:19 -0400

    setup DB for backend

---
+++ backend/README.md
@@ -0,0 +1,10 @@
+# Sherman Backend
+
+
+1. Design DB on dbdiagram.io and generate SQL code.
+
+![alt text](https://github.com/vaishvikmaisuria/Sherman/tree/main/backend/db/Database%20diagram.png?raw=true)
+
+2. Configure Postgres on Docker
+    1.
+Mobile App that helps roommates distribute expanses in a common living space.
---
+++ backend/db/Simple_Bank_Postgres.sql
@@ -0,0 +1,69 @@
+CREATE TYPE "currency" AS ENUM (
+  'USD',
+  'CAD',
+  'EUR'
+);
+
+CREATE TYPE "transaction_status" AS ENUM (
+  'pending',
+  'paid',
+  'deferred',
+  'not_paying',
+  'error'
+);
+
+CREATE TABLE "account" (
+  "id" bigserial PRIMARY KEY,
+  "owner_name" varchar NOT NULL,
+  "balance" decimal NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "amount" bigint NOT NULL,
+  "account_id" bigint
+);
+
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint,
+  "to_account_id" bigint,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "required_transaction" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint,
+  "to_account_id" bigint,
+  "amount" bigint,
+  "status" transaction_status,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "account" ("owner_name");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'amount can be negative or positive';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'amount must be positive';
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "account" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "account" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "account" ("id");
+
+ALTER TABLE "required_transaction" ADD FOREIGN KEY ("from_account_id") REFERENCES "account" ("id");
+
+ALTER TABLE "required_transaction" ADD FOREIGN KEY ("to_account_id") REFERENCES "account" ("id");
